cb92b1db62f73b6fd73257a564ff050f524708e4
Nick George <ngeorge@valorem.com>
2020-12-12 16:34:24 -0800

    Add in unit test project.

---
+++ Izzmo.BlazorChessApp.Tests/ChessBoardTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Izzmo.BlazorChessApp.Tests
+{
+    public class ChessBoardTest
+    {
+        [Fact]
+        public void ShouldHaveChessBoard()
+        {
+
+        }
+    }
+}
